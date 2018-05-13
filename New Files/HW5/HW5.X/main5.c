263739e6a684d7b6513c712f2c893b1f902672f7
mkf434 <michaelflynn2018@u.northwestern.edu>
2018-05-13 00:06:44 -0500

    Homework 5 is well underway, just put in the I2C functions from github and put in the helper function prototypes mentionedin the instructions

---
+++ New Files/HW5/HW5.X/main5.c
@@ -0,0 +1,133 @@
+/* 
+ * File:   main5.c
+ * Author: mkf434
+ *
+ * Created on May 12, 2018, 11:47 PM
+ */
+
+// Preprocessor Commands
+
+// DEVCFG3
+// USERID = No Setting
+#pragma config PMDL1WAY = OFF           // Peripheral Module Disable Configuration (Allow multiple reconfigurations)
+#pragma config IOL1WAY = OFF            // Peripheral Pin Select Configuration (Allow multiple reconfigurations)
+#pragma config FUSBIDIO = ON            // USB USID Selection (Controlled by the USB Module)
+#pragma config FVBUSONIO = ON           // USB VBUS ON Selection (Controlled by USB Module)
+
+// DEVCFG2
+#pragma config FPLLIDIV = DIV_2         // PLL Input Divider (2x Divider)
+#pragma config FPLLMUL = MUL_24         // PLL Multiplier (24x Multiplier)
+#pragma config UPLLIDIV = DIV_2         // USB PLL Input Divider (2x Divider)
+#pragma config UPLLEN = ON              // USB PLL Enable (Enabled)
+#pragma config FPLLODIV = DIV_2         // System PLL Output Clock Divider (PLL Divide by 2)
+
+// DEVCFG1
+#pragma config FNOSC = PRIPLL           // Oscillator Selection Bits (Primary Osc w/PLL (XT+,HS+,EC+PLL))
+#pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Disabled)
+#pragma config IESO = OFF               // Internal/External Switch Over (Disabled)
+#pragma config POSCMOD = HS             // Primary Oscillator Configuration (HS osc mode)
+#pragma config OSCIOFNC = OFF           // CLKO Output Signal Active on the OSCO Pin (Disabled)
+#pragma config FPBDIV = DIV_1           // Peripheral Clock Divisor (Pb_Clk is Sys_Clk/1)
+#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor Selection (Clock Switch Disable, FSCM Disabled)
+#pragma config WDTPS = PS1048576        // Watchdog Timer Postscaler (1:1048576)
+#pragma config WINDIS = OFF             // Watchdog Timer Window Enable (Watchdog Timer is in Non-Window Mode)
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled (SWDTEN Bit Controls))
+#pragma config FWDTWINSZ = WINSZ_25     // Watchdog Timer Window Size (Window Size is 25%)
+
+// DEVCFG0
+#pragma config JTAGEN = OFF             // JTAG Enable (JTAG Disabled)
+#pragma config ICESEL = ICS_PGx1        // ICE/ICD Comm Channel Select (Communicate on PGEC1/PGED1)
+#pragma config PWP = OFF                // Program Flash Write Protect (Disable)
+#pragma config BWP = OFF                // Boot Flash Write Protect bit (Protection Disabled)
+#pragma config CP = OFF                 // Code Protect (Protection Disabled)
+
+// Header Files
+
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Helper Function Prototypes
+
+void i2c_master_setup(void);
+void i2c_master_start(void);
+void i2c_master_restart(void);
+void i2c_master_send(unsigned char byte);
+unsigned char i2c_master_recv(void);
+void i2c_master_ack(int val);
+void i2c_master_stop(void);
+void initExpander();
+void setExpander(char pin, char level);
+char getExpander();
+
+// Main Function
+int main(int argc, char** argv) {
+    
+    
+
+    return (EXIT_SUCCESS);
+}
+
+// Helper Function Prototypes
+
+// I2C Master utilities, 100 kHz, using polling rather than interrupts
+// The functions must be callled in the correct order as per the I2C protocol
+// Change I2C1 to the I2C channel you are using
+// I2C pins need pull-up resistors, 2k-10k
+
+void i2c_master_setup(void) {
+  I2C2BRG = some number for 100kHz;            
+                                    // I2CBRG = [1/(2*Fsck) - PGD]*Pblck - 2 
+                                    // look up PGD for your PIC32
+  I2C2CONbits.ON = 1;               // turn on the I2C1 module
+}
+
+// Start a transmission on the I2C bus
+void i2c_master_start(void) {
+    I2C2CONbits.SEN = 1;            // send the start bit
+    while(I2C2CONbits.SEN) { ; }    // wait for the start bit to be sent
+}
+
+void i2c_master_restart(void) {     
+    I2C2CONbits.RSEN = 1;           // send a restart 
+    while(I2C2CONbits.RSEN) { ; }   // wait for the restart to clear
+}
+
+void i2c_master_send(unsigned char byte) {      // send a byte to slave
+  I2C2TRN = byte;                               // if an address, bit 0 = 0 for write, 1 for read
+  while(I2C2STATbits.TRSTAT) { ; }              // wait for the transmission to finish
+  if(I2C2STATbits.ACKSTAT) {                    // if this is high, slave has not acknowledged
+                                                // ("I2C2 Master: failed to receive ACK\r\n");
+  }
+}
+
+unsigned char i2c_master_recv(void) { // receive a byte from the slave
+    I2C2CONbits.RCEN = 1;             // start receiving data
+    while(!I2C2STATbits.RBF) { ; }    // wait to receive the data
+    return I2C2RCV;                   // read and return the data
+}
+
+void i2c_master_ack(int val) {        // sends ACK = 0 (slave should send another byte)
+                                      // or NACK = 1 (no more bytes requested from slave)
+    I2C2CONbits.ACKDT = val;          // store ACK/NACK in ACKDT
+    I2C2CONbits.ACKEN = 1;            // send ACKDT
+    while(I2C2CONbits.ACKEN) { ; }    // wait for ACK/NACK to be sent
+}
+
+void i2c_master_stop(void) {          // send a STOP:
+  I2C2CONbits.PEN = 1;                // comm is complete and master relinquishes bus
+  while(I2C2CONbits.PEN) { ; }        // wait for STOP to complete
+}
+
+void initExpander(){
+    
+}
+
+void setExpander(char pin, char level){
+    
+}
+
+char getExpander(){
+    
+}
+
