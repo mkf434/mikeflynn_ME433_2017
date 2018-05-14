aca4f2abee76ae404f11b39d836bef3c4f833d6d
mkf434 <michaelflynn2018@u.northwestern.edu>
2018-05-13 21:30:12 -0500

    Rolling right along, created the HW6 folder

---
+++ New Files/HW6/HW6.X/main6.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main6.c
+ * Author: mkf434
+ *
+ * Created on May 13, 2018, 9:29 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
