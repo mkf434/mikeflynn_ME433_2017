fd894df369ba8a4f56bb7a5c64b17042d5d4c746
mkf434 <michaelflynn2018@u.northwestern.edu>
2018-05-15 17:17:30 -0500

    Created Main file skeleton and started homework 7

---
+++ New Files/HW7/HW7.X/main7.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main7.c
+ * Author: mkf434
+ *
+ * Created on May 15, 2018, 5:09 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
