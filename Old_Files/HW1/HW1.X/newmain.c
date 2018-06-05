76fc56aefdc34fcaa1d8d97271fc043519c5ead4
mkf434 <michaelflynn2018@u.northwestern.edu>
2018-06-04 23:33:13 -0500

    Changed 'Old Files' also for continuity

---
+++ Old_Files/HW1/HW1.X/newmain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newmain.c
+ * Author: mkf434
+ *
+ * Created on April 21, 2017, 4:27 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
